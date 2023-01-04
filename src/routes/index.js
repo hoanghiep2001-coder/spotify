fc7809b81604affba8cf431c52d340b7d2960de4
DhHiep <duonghoanghiep.9b@gmail.com>
2023-01-05 01:32:40 +0700

    customize cra, webpack, layout

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import Home from "../pages/Home";
+import Library from "../pages/Library";
+
+const publicRoutes = [
+  { path: "/", component: Home },
+  { path: "/library", component: Library },
+];
+
+export default publicRoutes;
